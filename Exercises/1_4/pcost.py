172f0d30c625e2d276ae7ccd020947db8c0bee92
Ryan Luker <ryanluker@pm.me>
2023-08-26 16:57:16 -0700

    Finish exercise 1_4

--- Exercises/1_4/pcost.py
+++ Exercises/1_4/pcost.py
@@ -1,32 +1,17 @@
 # pcost.py
-# read in data file and perform cost multiplication
+# read in data file and perform cost multiplication.
+# contains a single helper function for achieving the above.
 
-import pprint
+def portfolio_cost(filename):
+    total_equity = 0
+    relative_path = "../../"
+    with open(relative_path + filename, "r") as f:
+        for company in f:
+            # process company info by splitting on empty string
+            values = company.split()
+            shares = int(values[1])
+            price = float(values[2])
+            total_equity += (shares * price)
 
-company_dict = {}
-with open("../../Data/portfolio.dat", "r") as f:
-    for company in f:
-        # process company info by splitting on empty string
-        values = company.strip("\n").split(" ")
-        # constructs a dict obj of key values
-        # ticker: {shares: int, price: float}
-        ticker = values[0]
-        shares = int(values[1])
-        price = float(values[2])
-        total_equity = round(shares * price, 2)
-        company_dict[ticker] = {
-            "shares": shares,
-            "price": price,
-            "total_equity": total_equity
-        }
-
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(company_dict)
-
-# Compute total portfolio equity and print
-total_port_equity = 0
-for company in company_dict.values():
-    total_port_equity += company.get("total_equity")
-
-pp.pprint(total_port_equity)
+    return total_equity
 
